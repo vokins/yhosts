71954edb2b295f2962fa9cf87854e2fee7e3fd50
vokins <vokins@gmail.com>
2016-01-20 18:17:34 +0800

    完善数据分类

---
+++ @M.bat
@@ -0,0 +1,56 @@
+:downgrd
+"%~dp0lib\wget.exe" -c --no-check-certificate -O grd.txt https://raw.githubusercontent.com/racaljk/hosts/master/hosts
+"%~dp0lib\sed.exe" -i "1,18d" grd.txt
+"%~dp0lib\sed.exe" -i "s/\t/ /g" grd.txt
+"%~dp0lib\sed.exe" -i "s/[ ]\{2,\}/ /g" grd.txt
+"%~dp0lib\sed.exe" -i "/googlesyndication/d" grd.txt
+"%~dp0lib\sed.exe" -i "/googleadservices/d" grd.txt
+"%~dp0lib\sed.exe" -i "/127.0.0.1/d" grd.txt
+"%~dp0lib\sed.exe" -i "/^$/d" grd.txt
+"%~dp0lib\sed.exe" -i "/^#/d" grd.txt
+"%~dp0lib\sed.exe" -i "1i\@redirect" grd.txt
+
+ver=17:26 2016/1/20/ÖÜÈý
+SetLocal EnableExtensions
+SetLocal EnableDelayedExpansion
+set str=%date:~0,4%%date:~5,2%00
+call :del
+call :bat
+call :version
+call :xunlei
+call :data
+sed -i "s/127.0.0.1/0.0.0.0/g" 0.txt
+ping -n 5 127.0.0.1
+winhosts.bat
+exit
+
+:del
+del /f Version.txt Xunlei.txt bat.txt grd.txt 1A.txt
+goto :eof
+
+:bat
+echo title yhosts >bat.txt
+echo more +5 %%~fs0^>%%systemroot%%\system32\drivers\etc\hosts >>bat.txt
+echo notepad %%windir%%\system32\drivers\etc\hosts >>bat.txt
+echo goto :eof >>bat.txt
+goto :eof
+
+:version
+echo.>Version.txt
+echo ;version=%date:~0,4%%date:~5,2%%date:~8,2%%TIME:~0,2%%TIME:~3,2%>>Version.txt
+rem echo ;version=%time% %date%>>Version.txt
+echo ;url=https://github.com/vokins/yhosts >>Version.txt
+goto :eof
+
+:xunlei
+set /a str+=1
+echo 0.0.0.0 %str%.biz5.sandai.net >>xunlei.txt
+echo 0.0.0.0 %str%.float.sandai.net >>xunlei.txt
+echo 0.0.0.0 %str%.logic.cpm.cm.sandai.net >>xunlei.txt
+if not %str%==%date:~0,4%%date:~5,2%31 (goto Xunlei)
+goto :eof
+
+:data
+set files=bat.txt Version.txt redirect.txt grd.txt hosts.txt xunlei.txt site.txt mobile.txt apps.txt soft.txt cps.txt daohang.txt down.txt errorpage.txt
+for %%a in (%files%) do (type "%%a">>1A.txt)
+goto :eof
--- winhosts.bat
+++ winhosts.bat
@@ -4,6 +4,6 @@ Setlocal enabledelayedexpansion
 TAKEOWN /F "%windir%\System32\drivers\etc" /A
 echo y|CACLS %windir%\system32\drivers\etc/t /C /p everyone:f
 rem icacls "%windir%\System32\drivers\etc" /grant "NT AUTHORITY\NetworkService":RX
-copy /y "%~dp0hosts.txt" "%windir%\system32\drivers\etc\hosts"
+copy /y "%~dp01A.txt" "%windir%\system32\drivers\etc\hosts"
 ipconfig /flushdns
 exit
