87de6c5d799562e2f36ed10bd30c30588637d81e
vokins <vokins@gmail.com>
2016-02-06 17:30:39 +0800

    合并soft到hosts.txt合并errorpage数据

--- 1win.bat
+++ 1win.bat
@@ -4,5 +4,6 @@ TAKEOWN /F "%windir%\System32\drivers\etc" /A
 echo y|CACLS %windir%\system32\drivers\etc/t /C /p everyone:f
 rem icacls "%windir%\System32\drivers\etc" /grant "NT AUTHORITY\NetworkService":RX
 copy /y "%~dphosts.txt" "%windir%\system32\drivers\etc\hosts"
+pause
 ipconfig /flushdns
 exit
