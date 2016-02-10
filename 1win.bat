a5f10217de8b421e395031e38e449292ad3028bf
vokins <vokins@gmail.com>
2016-02-10 22:49:44 +0800

    依照simpleu hosts的站点列表,全部重新分析嗅探网页广告数据.

--- 1win.bat
+++ 1win.bat
@@ -3,6 +3,8 @@ Setlocal enabledelayedexpansion
 TAKEOWN /F "%windir%\System32\drivers\etc" /A
 echo y|CACLS %windir%\system32\drivers\etc/t /C /p everyone:f
 rem icacls "%windir%\System32\drivers\etc" /grant "NT AUTHORITY\NetworkService":RX
-copy /y "%~dp0hosts.txt" "%windir%\system32\drivers\etc\hosts"
+copy /a hosts.txt + grd.txt hbhosts.txt
+copy /y "%~dp0hbhosts.txt" "%windir%\system32\drivers\etc\hosts"
 ipconfig /flushdns
+del /f hbhosts.txt
 exit
