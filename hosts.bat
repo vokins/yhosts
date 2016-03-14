d245e31079d9a2aaa4b46c6f20658c69645608ef
vokins <vokins@gmail.com>
2016-03-14 13:32:28 +0800

    完善数据，添加win下批处理工具

---
+++ hosts.bat
@@ -0,0 +1,51 @@
+@echo off
+ver=13:31 2016/3/14/周一
+SetLocal EnableExtensions
+SetLocal EnableDelayedExpansion
+set str=%date:~0,4%%date:~5,2%00
+call :del
+call :xunlei
+call :downgrd
+call :winhosts
+call :del
+cls
+msg %username% /time:3 "hosts文件已成功更新！"
+exit
+
+:del
+del /f 1.txt xunlei.txt hbhosts.txt 2.txt
+goto :eof
+
+:xunlei
+set /a str+=1
+echo 127.0.0.1 %str%.biz5.sandai.net>>xunlei.txt
+echo.
+echo 127.0.0.1 %str%.float.sandai.net>>xunlei.txt
+echo.
+echo 127.0.0.1 %str%.logic.cpm.cm.sandai.net>>xunlei.txt
+if not %str%==%date:~0,4%%date:~5,2%31 (goto Xunlei)
+goto :eof
+
+:downgrd
+rem "%~dp0lib\wget.exe" -c --no-check-certificate
+"%~dp0lib\curl.exe" https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt > 2.txt
+"%~dp0lib\curl.exe" https://raw.githubusercontent.com/racaljk/hosts/master/hosts > 1.txt
+"%~dp0lib\sed.exe" -i "1,18d" 1.txt
+"%~dp0lib\sed.exe" -i "s/\t/ /g" 1.txt
+"%~dp0lib\sed.exe" -i "s/[ ]\{2,\}/ /g" 1.txt
+"%~dp0lib\sed.exe" -i "/googlesyndication/d" 1.txt
+"%~dp0lib\sed.exe" -i "/googleadservices/d" 1.txt
+"%~dp0lib\sed.exe" -i "/127.0.0.1/d" 1.txt
+"%~dp0lib\sed.exe" -i "/^$/d" 1.txt
+"%~dp0lib\sed.exe" -i "/^#/d" 1.txt
+"%~dp0lib\sed.exe" -i "1i\@racaljk/hosts" 1.txt
+goto :eof
+
+:winhosts
+TAKEOWN /F "%windir%\System32\drivers\etc" /A
+echo y|CACLS %windir%\system32\drivers\etc/t /C /p everyone:f
+rem icacls "%windir%\System32\drivers\etc" /grant "NT AUTHORITY\NetworkService":RX
+copy /a 2.txt + 1.txt hbhosts.txt
+copy /y "%~dp0hbhosts.txt" "%windir%\system32\drivers\etc\hosts"
+ipconfig /flushdns
+goto :eof
