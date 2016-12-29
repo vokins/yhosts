7dddefac53da974374aad8c1b9d2a2962e904c30
vokins <vokins@gmail.com>
2016-12-29 23:19:48 +0800

    完善各项mac shell

---
+++ mac/hosts.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd /tmp
+rm *.temp
+curl "https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt" -o "yhosts.temp"
+curl "https://raw.githubusercontent.com/racaljk/hosts/master/hosts " -o "grd.temp"
+sed -i "" "1,13d" grd.temp
+sed -i "" "/googlesyndication/d" grd.temp
+sed -i "" "/googleadservices/d" grd.temp
+sed -i "" "/^#/d" grd.temp
+sed -i "" "/^$/d" grd.temp
+cat grd.temp|tr  "\t" " " >grd1.temp
+cat grd1.temp >> yhosts.temp
+sudo cp yhosts.temp /etc/hosts
+rm *.temp
+sudo dscacheutil -flushcache
+sudo killall -HUP mDNSResponder
+say yHosts updated
+echo "Succed..."
