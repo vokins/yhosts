aa763c075ad4feb603f78ecec7fc5a50d87d6d67
vokins <vokins@gmail.com>
2017-02-11 18:26:57 +0800

    4url

--- mac/hosts.sh
+++ mac/hosts.sh
@@ -4,14 +4,16 @@ cd /tmp
 rm *.temp
 curl "https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt" -o "yhosts.temp"
 curl "https://raw.githubusercontent.com/racaljk/hosts/master/hosts " -o "grd.temp"
+curl "https://raw.githubusercontent.com/sy618/hosts/master/p " -o "p.temp"
+curl "https://raw.githubusercontent.com/sy618/hosts/master/y " -o "y.temp"
 sed -i "" "1,13d" grd.temp
 sed -i "" "/googlesyndication/d" grd.temp
 sed -i "" "/googleadservices/d" grd.temp
 sed -i "" "/^#/d" grd.temp
 sed -i "" "/^$/d" grd.temp
 cat grd.temp|tr  "\t" " " >grd1.temp
-cat grd1.temp >> yhosts.temp
-sudo cp yhosts.temp /etc/hosts
+cat grd1.temp p.temp y.temp yhosts.temp >> new.temp
+sudo cp new.temp /etc/hosts
 rm *.temp
 sudo dscacheutil -flushcache
 sudo killall -HUP mDNSResponder
