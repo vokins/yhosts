2e8843c03b82cda37f7c41b62208ae4c153e5326
vokins <vokins@gmail.com>
2017-05-30 21:31:34 +0800

    完善ru广告联盟，完善rus（site）数据，完善运营商劫持数据，继续缩减Sohu数据。删除sitefor数据内部多余的空行，完善新浪广告数据（新浪娱乐手机版），完善阿起开发的筛选器的白名单数据，完善vip目录的广告图片屏蔽img数据。

--- mac/hosts.sh
+++ mac/hosts.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
+#  hosts.sh
+#  Created on 2017/5/30.
+#
 cd /tmp
 rm *.temp
 curl "https://raw.githubusercontent.com/vokins/yhosts/master/hosts.txt" -o "yhosts.temp"
 curl "https://raw.githubusercontent.com/racaljk/hosts/master/hosts " -o "grd.temp"
-curl "https://raw.githubusercontent.com/sy618/hosts/master/p " -o "p.temp"
-curl "https://raw.githubusercontent.com/sy618/hosts/master/y " -o "y.temp"
+curl "https://raw.githubusercontent.com/ss916/bug/master/log/bug " -o "ss916.temp"
 sed -i "" "1,13d" grd.temp
-sed -i "" "/googlesyndication/d" grd.temp
-sed -i "" "/googleadservices/d" grd.temp
-sed -i "" "/^#/d" grd.temp
-sed -i "" "/^$/d" grd.temp
-cat grd.temp|tr  "\t" " " >grd1.temp
-cat grd1.temp p.temp y.temp yhosts.temp >> new.temp
-sudo cp new.temp /etc/hosts
+cat grd.temp ss916.temp >> new.temp
+sed -i "" "/googlesyndication/d" new.temp
+sed -i "" "/googleadservices/d" new.temp
+sed -i "" "/^#/d" new.temp
+sed -i "" "/^$/d" new.temp
+cat new.temp|tr  "\t" " " >new1.temp
+cat yhosts.temp new1.temp >> new2.temp
+sudo cp new2.temp /etc/hosts
 rm *.temp
 sudo dscacheutil -flushcache
 sudo killall -HUP mDNSResponder
