71f5db34d87cbea85a8c9d246cf1450fb87b007b
vokins <vokins@gmail.com>
2017-06-16 23:09:26 +0800

    fix error

--- miwifi/mi.sh
+++ miwifi/mi.sh
@@ -1,1 +1,3 @@
-cd /etc && rm -rf /etc/hosts && curl -o hosts -k https://raw.githubusercontent.com/vokins/yhosts/master/hosts && cd /etc/dnsmasq.d && curl -o dnsfq.conf -k https://raw.githubusercontent.com/ss916/bug/master/dnsmasq/dnsfq && curl -o ip.conf -k https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf && curl -o union.conf -k https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf && /etc/init.d/dnsmasq restart
+cd /etc && curl -o hosts -k https://raw.githubusercontent.com/vokins/yhosts/master/hosts && cd /etc/dnsmasq.d && curl -o dnsfq.conf -k https://raw.githubusercontent.com/ss916/bug/master/dnsmasq/dnsfq && curl -o ip.conf -k https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf && curl -o union.conf -k https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf && /etc/init.d/dnsmasq restart
+
+
