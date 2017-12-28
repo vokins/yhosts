4da6daa6eab253f4e9c0832d89b81a7b2c34af0b
sy618 <ggsyz619@gmail.com>
2017-12-28 11:50:55 +0800

    修改ADhosts-dnsmasq.sh脚本

--- mac/ADhosts-dnsmasq.sh
+++ mac/ADhosts-dnsmasq.sh
@@ -5,12 +5,11 @@
 cd /storage/sdcard1/dnsmasq
 
 #下载AD hosts与AD dnsmasq文件，
-#curl -o hosts -k https://raw.githubusercontent.com/vokins/yhosts/master/hosts
-#curl -o union.conf -k https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf
+curl -o hosts -k https://raw.githubusercontent.com/vokins/yhosts/master/hosts
+curl -o union.conf -k https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf
 
-#提取union.conf里的域名
-awk -F/ '{print $2}' union.conf > domain
-sed -i 's#^\.##g' domain
+#去掉union.conf里注释行与空白行、提取域名、去掉行首的“.”，输出domain
+cat union.conf | sed '/#\|^ *$/d' | awk -F/ '{print $2}' | sed  's#^\.##g' > domain
 
 #使用循环来批量删除ADhosts里包含domain文件里的域名
 for abc in $(cat domain)
