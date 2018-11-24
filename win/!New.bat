36ba9bc59fb1f6fa46679426396209223794bbdd
vokins <vokins@gmail.com>
2018-11-24 19:45:52 +0800

    #js.meitulu.com #m.11kt.cn #m.2kxs.com #m.3dllc.cc #m.76long.com #m.bequge.com #m.gulizw.com #m.x81zw.com #m.zwda.com #s.dzz8.com
    127.0.0.1 vd.l.qq.com 腾讯视频vip，旧的vip电视剧无法播放

--- win/!New.bat
+++ win/!New.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-rem 13:50 2018/11/20/周二
+rem 22:09 2018/11/23/周五
 cd /d "%~dp0"
 Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
 Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行！&&Pause >nul&&Exit)
@@ -844,7 +844,8 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\DandelionSetup.exe" /v Debugger /t REG_SZ /d "p" /f
 rem Pops
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\KuaizipUpdate.exe" /v Debugger /t REG_SZ /d "p" /f
-
+rem Glarysoft
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\GUDownloader.exe" /v Debugger /t REG_SZ /d "p" /f
 
 cls
 echo 更新策略
