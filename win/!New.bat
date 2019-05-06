6b94015c58993dc812f3e14ff6ebc10055c0e239
vokins <vokins@gmail.com>
2019-05-06 11:15:01 +0800

    #m.123danmei.cc #m.31wxw8.com #m.37zw.net #m.50zww.com #m.58xs.tw #m.beijingaishu.net #m.bequgezw.com #m.biqubao.com #m.biyuwu.cc #m.bxwx9.org #m.fhxiaoshuo.com #m.qianrenge.cc #m.shuhuangge.org #m.shumil.com #m.shushu8.com #m.suimeng.la #m.weilaitianwang.com #m.wenxuemi.com #m.xuanjiezhimen.org #m.yikanxiaoshuo.net #m.zhenben.cc #m.zhuaji.org #m.zwdu.com

--- win/!New.bat
+++ win/!New.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-rem 15:39 2019/4/23
+rem 10:30 2019/5/6
 cd /d "%~dp0"
 Rd "%WinDir%\system32\test_permissions" >NUL 2>NUL
 Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行！&&Pause >nul&&Exit)
@@ -875,8 +875,6 @@ sc config lfsvc start=disabled
 echo Phone Service
 sc config PhoneSvc start=disabled
 
-echo Shell Hardware Detection
-sc config ShellHWDetection start=disabled
 echo Remote Registry(远程修改注册表)
 sc config RemoteRegistry start=disabled
 echo Superfetch
